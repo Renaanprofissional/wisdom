f9b77fd083ace6235e34cf345df1d107916f2f3f
Renaanprofissional <renaan.profissional@gmail.com>
2026-04-01 21:02:25 -0300

    fix: prisma ajust

--- _lib/prisma.ts
+++ _lib/prisma.ts
@@ -1,26 +1,15 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "../app/generated/prisma/client"
-
-declare global {
-  var cachedPrisma: PrismaClient
-}
-
-let prisma: PrismaClient
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient({
-    adapter: new PrismaPg({
-      connectionString: process.env.DATABASE_URL!,
-    }),
-  })
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient({
-      adapter: new PrismaPg({
-        connectionString: process.env.DATABASE_URL!,
-      }),
-    })
-  }
-  prisma = global.cachedPrisma
-}
-
-export const db = prisma
+import { PrismaClient } from "../app/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
