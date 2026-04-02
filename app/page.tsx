c32bd85cfa4574d7fe6633d2c7eb0e969f8ac296
Renaanprofissional <renaan.profissional@gmail.com>
2026-04-02 19:04:13 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Wisdom</h1>
+      <Button>Login</Button>
     </div>
   );
 }
