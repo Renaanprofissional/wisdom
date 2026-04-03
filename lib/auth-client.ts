a899d79a212c53939b11cda2253468f9d9f967cd
Renaanprofissional <renaan.profissional@gmail.com>
2026-04-03 10:50:23 -0300

    fix: ajust auth-client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,8 @@
-"use client";
-
+// lib/auth-client.ts
 import { createAuthClient } from "better-auth/react";
 
-// Cria o cliente de autenticação
-export const authClient = createAuthClient({});
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTERAUTH_URL, // URL completa do servidor BetterAuth
+});
+
+export const { signIn, signOut, useSession, getSession } = authClient;
